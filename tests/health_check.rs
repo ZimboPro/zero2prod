fab74495989b692251027b41214bb90cbcbfb3bd
Leslie <dejagerleslie@gmail.com>
2023-04-08 16:43:45 +0200

    cargo fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,5 @@
 use std::net::TcpListener;
 
-
-
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
