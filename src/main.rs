f526dc13074a88e79a90548caf9a948ad129623f
Leslie <dejagerleslie@gmail.com>
2023-04-08 16:38:55 +0200

    Initial server endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let port = 8000;
+    let listener = TcpListener::bind(format!("127.0.0.1:{port}"))
+        .expect(format!("Port {port} already is use").as_str());
+    run(listener)?.await
 }
