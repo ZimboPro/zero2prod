fed8a887fe0f96ede1ed1fa74310712b3770748f
Leslie <dejagerleslie@gmail.com>
2023-04-10 11:20:48 +0200

    Add database storage and connection

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use sqlx::{PgConnection, Connection, PgPool};
+use zero2prod::{startup::run, configuration::get_configuration};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let port = 8000;
-    let listener = TcpListener::bind(format!("127.0.0.1:{port}"))
-        .unwrap_or_else(|_| panic!("Port {port} already is use"));
-    run(listener)?.await
+    let config = get_configuration().expect("Failed to read configuration");
+    let connection = PgPool::connect(
+        &config.database.connection_string()
+        )
+        .await
+        .expect("Failed to connect to Postgres.");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", config.application_port))
+        .unwrap_or_else(|_| panic!("Port {} already is use", config.application_port));
+    run(listener, connection)?.await
 }
