d4da9b1cb466ef524d82499582b20381d5893dd4
Leslie <dejagerleslie@gmail.com>
2023-04-24 18:04:08 +0200

    Add tracing logs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
+use secrecy::ExposeSecret;
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let config = get_configuration().expect("Failed to read configuration");
-    let connection = PgPool::connect(&config.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", config.application_port))
-        .unwrap_or_else(|_| panic!("Port {} already is use", config.application_port));
-    run(listener, connection)?.await
+
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+    
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool =
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
+
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))
+        .unwrap_or_else(|_| panic!("Port {} already is use", configuration.application_port));
+    run(listener, connection_pool)?.await
 }
