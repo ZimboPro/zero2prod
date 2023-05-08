888999099903577d7b425927dcd3dfee206c50a6
Leslie <dejagerleslie@gmail.com>
2023-05-08 11:26:46 +0200

    Add configuration - cargo fmt

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -7,18 +7,18 @@ use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    email: String,
-    name: String,
+  email: String,
+  name: String,
 }
 
 impl TryFrom<FormData> for NewSubscriber {
-    type Error = String;
+  type Error = String;
 
-    fn try_from(value: FormData) -> Result<Self, Self::Error> {
-        let name = SubscriberName::parse(value.name)?;
-        let email = SubscriberEmail::parse(value.email)?;
-        Ok(Self { email, name })
-    }
+  fn try_from(value: FormData) -> Result<Self, Self::Error> {
+    let name = SubscriberName::parse(value.name)?;
+    let email = SubscriberEmail::parse(value.email)?;
+    Ok(Self { email, name })
+  }
 }
 
 #[tracing::instrument(
@@ -30,40 +30,40 @@ impl TryFrom<FormData> for NewSubscriber {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let new_subscriber = match form.0.try_into() {
-        Ok(subscriber) => subscriber,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
+  let new_subscriber = match form.0.try_into() {
+    Ok(subscriber) => subscriber,
+    Err(_) => return HttpResponse::BadRequest().finish(),
+  };
 
-    match insert_subscriber(&pool, &new_subscriber).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }
+  match insert_subscriber(&pool, &new_subscriber).await {
+    Ok(_) => HttpResponse::Ok().finish(),
+    Err(_) => HttpResponse::InternalServerError().finish(),
+  }
 }
 
 #[tracing::instrument(
-    name = "Saving new subscriber details in the database",
-    skip(new_subscriber, pool)
+  name = "Saving new subscriber details in the database",
+  skip(new_subscriber, pool)
 )]
 pub async fn insert_subscriber(
-    pool: &PgPool,
-    new_subscriber: &NewSubscriber,
+  pool: &PgPool,
+  new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        r#"
+  sqlx::query!(
+    r#"
       INSERT INTO subscriptions (id, email, name, subscribed_at)
       VALUES ($1, $2, $3, $4)
       "#,
-        Uuid::new_v4(),
-        new_subscriber.email.as_ref(),
-        new_subscriber.name.as_ref(),
-        Utc::now()
-    )
-    .execute(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
-    Ok(())
+    Uuid::new_v4(),
+    new_subscriber.email.as_ref(),
+    new_subscriber.name.as_ref(),
+    Utc::now()
+  )
+  .execute(pool)
+  .await
+  .map_err(|e| {
+    tracing::error!("Failed to execute query: {:?}", e);
+    e
+  })?;
+  Ok(())
 }
