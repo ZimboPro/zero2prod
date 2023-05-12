103434e4dd8280ac1330a28f7ba0c6c4e2dc6591
Leslie <dejagerleslie@gmail.com>
2023-05-12 09:27:53 +0200

    Remove old imports

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,6 @@
-use actix_web::{error::InternalError, web, HttpResponse, ResponseError};
+use actix_web::{error::InternalError, web, HttpResponse};
 use hmac::{Hmac, Mac};
-use reqwest::{header::LOCATION, StatusCode};
+use reqwest::{header::LOCATION};
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
