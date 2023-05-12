757460b423decf1f1b66b2b184422152e4fcfecc
Leslie <dejagerleslie@gmail.com>
2023-05-12 09:26:49 +0200

    Remove unneeded code

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -73,22 +73,3 @@ impl std::fmt::Debug for LoginError {
     error_chain_fmt(self, f)
   }
 }
-
-impl ResponseError for LoginError {
-  fn error_response(&self) -> HttpResponse {
-    let query_string = format!("error={}", urlencoding::Encoded::new(self.to_string()));
-    let secret: &[u8] = todo!();
-    let hmac_tag = {
-      let mut mac = Hmac::<sha2::Sha256>::new_from_slice(secret).unwrap();
-      mac.update(query_string.as_bytes());
-      mac.finalize().into_bytes()
-    };
-    HttpResponse::build(self.status_code())
-      .insert_header((LOCATION, format!("/login?{query_string}&tag={hmac_tag:x}")))
-      .finish()
-  }
-
-  fn status_code(&self) -> StatusCode {
-    StatusCode::SEE_OTHER
-  }
-}
