74765d23cf2bf63e6f2cb989c987244292de724f
Leslie <dejagerleslie@gmail.com>
2023-05-15 14:50:19 +0200

    Add admin dashboard

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -7,6 +7,7 @@ use sqlx::PgPool;
 use crate::{
   authentication::{validate_credentials, Credentials},
   routes::error_chain_fmt,
+  session_state::TypedSession,
 };
 
 #[derive(serde::Deserialize)]
@@ -18,6 +19,7 @@ pub struct FormData {
 pub async fn login(
   form: web::Form<FormData>,
   pool: web::Data<PgPool>,
+  session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
   let credentials = Credentials {
     username: form.0.username,
@@ -27,9 +29,13 @@ pub async fn login(
   match validate_credentials(credentials, &pool).await {
     Ok(user_id) => {
       tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+      session.renew();
+      session
+        .insert_user_id(user_id)
+        .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
       Ok(
         HttpResponse::SeeOther()
-          .insert_header((LOCATION, "/"))
+          .insert_header((LOCATION, "/admin/dashboard"))
           .finish(),
       )
     }
@@ -40,13 +46,17 @@ pub async fn login(
           LoginError::UnexpectedError(e.into())
         }
       };
-      FlashMessage::error(e.to_string()).send();
-      let response = HttpResponse::SeeOther()
-        .insert_header((LOCATION, "/login"))
-        .finish();
-      Err(InternalError::from_response(e, response))
+      Err(login_redirect(e))
     }
   }
+}
+
+fn login_redirect(e: LoginError) -> InternalError<LoginError> {
+  FlashMessage::error(e.to_string()).send();
+  let response = HttpResponse::SeeOther()
+    .insert_header((LOCATION, "/login"))
+    .finish();
+  InternalError::from_response(e, response)
 }
 
 #[derive(thiserror::Error)]
